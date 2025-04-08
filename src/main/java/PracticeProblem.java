6f6906f64b935f0c13b1ea85f3e08bdff2452971
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 05:58:45 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,34 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
 	
-
+	public static boolean validIndex(int[] intArray, int num1){
+		
+		try {
+			int num2 = intArray[num1];
+			return true;
+			}
+			catch (ArrayIndexOutOfBoundsException e){
+				return false;
+		}
+	}
+	
+	public static int divide(int firstnum, int secondnum) {
+       
+	    try {
+        	return firstnum / secondnum;
+        		} catch (ArithmeticException e) {
+            		return 0;
+        }
+    }
+	
+	public static int safeConvertStringtoInt(String string1){
+		
+		try{
+			return Integer.parseInt(string1);
+			}
+				catch(NumberFormatException e){
+					return 0;
+		}
+	}
 }
